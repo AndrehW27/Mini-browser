1c94760d78b7b4a02c1e57f6a80aa4df962795db
André Willian Gorgo de Carvalho <andrehwillian@hotmail.com>
2025-03-04 02:27:47 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const closeTab = () => 
+  console.log('Close tab');
+
+const goBack = () => 
+  console.log('goBack');
+  
+const goForward = () => 
+  console.log('goForward');
+  
+const refreshPage = () => 
+  console.log('refreshPage');
+  
+const goHome = () => 
+  console.log('goHome');
+  
+const minimize = () => 
+  console.log('Minimize');
+  
+const maxmize = () => 
+  console.log('Maxmize');
+
+const closewindow = () => 
+  console.log('closewindow');
+
+const newTab = () => 
+  console.log('newTab');
+
+const history = () => 
+  console.log('history');
