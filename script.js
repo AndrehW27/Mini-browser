db49bcbc49683ae10bb49cfee8cc4c7572339218
AndrehW27 <andrehwillian@hotmail.com>
2025-03-05 14:20:06 -0300

    historico v1.0

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Registrar o horario
+const now = new Date();
+const day = String(now.getDate()).padStart(2, '0'); // Add leading zero if needed
+const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+
 const closeTab = () => {
   console.log('Close tab');
   document.getElementById('containerId').style.display = 'none';
@@ -28,6 +35,35 @@ const goHome = () => {
     document.getElementById('contentImgId').src = sites[0].img;
     document.getElementById('tabIconId').src = sites[0].icon;  
     document.getElementById('urlInputId').value = sites[0].url;  
+
+    let url = sites[0].url;
+    let hour = day+'/'+month+' às '+hours+':'+minutes;
+
+    let homeSite = {
+      url: url,
+      hour: hour
+    };
+
+    //Preenche o history stack
+    history.pushHistory(homeSite);
+
+    //Atualiza lista visivel do histórico (criando um <p>)  
+    const list = document.getElementById('historyListId');
+
+    //HORA
+    const hourParagraph = document.createElement('p');
+    hourParagraph.id = 'new-hour-paragraph'
+    hourParagraph.classList.add('hora');
+    hourParagraph.textContent = hour;
+    list.prepend(hourParagraph);
+
+    //URL 
+    const urlParagraph = document.createElement('p');
+    urlParagraph.id = 'new-paragraph'
+    urlParagraph.classList.add('history-site');
+    urlParagraph.textContent = url;
+    list.prepend(urlParagraph);
+    
 }
   
 const minimize = () => {
@@ -52,10 +88,34 @@ const newTab = () => {
 }
   
 const seeHistory = () => {
-    console.log('history');
-    document.getElementById('historyListId').style.visibility = 'visible';
+    // console.log('history');
+    // console.log('visibility: ', document.getElementById('historyListId').style.visibility);
+    if (document.getElementById('historyListId').style.visibility === 'visible') {
+      document.getElementById('historyListId').style.visibility = 'hidden';
+    }
+    else{
+      document.getElementById('historyListId').style.visibility = 'visible';
+    }
+    
 }
 
+const popHistory = () => {
+  history.popHistory();
+  console.log("history: ", history);  
+
+  const list = document.getElementById('historyListId');
+  const url = document.getElementById('new-paragraph');
+  const hour = document.getElementById('new-hour-paragraph');
+  list.removeChild(url);
+  list.removeChild(hour);
+}
+
+const clearHistory = () => {
+  history.clearHistory();
+  console.log("history: ", history);  
+
+  document.getElementById('historyListId').innerHTML = '';  
+}
 // ==============================
 //SITES/HISTORY OBJECT LISTS
 // ==============================
@@ -70,8 +130,14 @@ console.log("Sites: ", sites);
 
 //HISTORY
 class HistoryStack {
+  // constructor(url, hour) {
+  //   this.stack = [
+  //     this.url = url,
+  //     this.hour = hour
+  //   ];
+  // }
   constructor() {
-    this.stack = [];
+    this.stack = [];  
   }
 
   pushHistory(site) {
@@ -99,7 +165,6 @@ const history = new HistoryStack();
 
 console.log("history: ", history);
 
-
 //Setando a home para google
 document.getElementById('urlInputId').value = sites[0].url;
 document.getElementById('contentImgId').src = sites[0].img;
@@ -114,7 +179,15 @@ document.getElementById('urlInputId').addEventListener('click', () => {
 document.getElementById('urlInputId').addEventListener('keydown', (event) => {
   if (event.key === 'Enter') { // Check if the key pressed is Enter
     changeUrl();
-    console.log('Enter key pressed');
+    document.getElementById('historyListId').style.visibility = 'visible';
+    // console.log('Enter key pressed');
+    // console.log('style.visibility: ', document.getElementById('historyListId').style.visibility);
+
+    // if (document.getElementById('historyListId').style.visibility === 'hidden') {
+    //   document.getElementById('historyListId').style.visibility = 'visible';
+    //   console.log('entrei aqui!');
+      
+    // }
   }
 });
 
@@ -125,27 +198,78 @@ const changeUrl = () => {
     let currentValue = input.value;
 
     if (currentValue.includes('goog')) {
-      input.value = currentValue.replace("goog", sites[0].url);
+      input.value = currentValue.replace(currentValue, sites[0].url);
       document.getElementById('titleId').innerText = sites[0].title;
       document.getElementById('contentImgId').src = sites[0].img;
       document.getElementById('tabIconId').src = sites[0].icon;  
     } else if (currentValue.includes('face')) {
-      input.value = currentValue.replace("face", sites[1].url);
+      input.value = currentValue.replace(currentValue, sites[1].url);
       document.getElementById('titleId').innerText = sites[1].title;
       document.getElementById('contentImgId').src = sites[1].img;
       document.getElementById('tabIconId').src = sites[1].icon;   
     } else if (currentValue.includes('insta')) {
-      input.value = currentValue.replace("insta", sites[2].url);
+      input.value = currentValue.replace(currentValue, sites[2].url);
       document.getElementById('titleId').innerText = sites[2].title;
       document.getElementById('contentImgId').src = sites[2].img;
       document.getElementById('tabIconId').src = sites[2].icon;  
     } else if (currentValue.includes('you')) {
-      input.value = currentValue.replace("you", sites[3].url);
+      input.value = currentValue.replace(currentValue, sites[3].url);
       document.getElementById('titleId').innerText = sites[3].title;
       document.getElementById('contentImgId').src = sites[3].img;
       document.getElementById('tabIconId').src = sites[3].icon;  
     }
-    history.pushHistory(document.getElementById('urlInputId').value);
+
+
+    //Rsgitro de site no historico
+    let url = input.value;
+    let hour = day+'/'+month+' às '+hours+':'+minutes;
+
+    let historySite = {
+      url: url,
+      hour: hour
+    };
+
+    //Preenche o history stack
+    history.pushHistory(historySite);
+
+    //Atualiza lista visivel do histórico (criando um <p>)  
+    const list = document.getElementById('historyListId');
+
+    //HORA
+    const hourParagraph = document.createElement('p');
+    hourParagraph.id = 'new-hour-paragraph'
+    hourParagraph.classList.add('hora');
+    hourParagraph.textContent = hour;
+    list.prepend(hourParagraph);
+
+    //URL 
+    const urlParagraph = document.createElement('p');
+    urlParagraph.id = 'new-paragraph'
+    urlParagraph.classList.add('history-site');
+    urlParagraph.textContent = url;
+    list.prepend(urlParagraph);
+
+
+
+    // document.getElementById('historySiteId').innerHTML = url;
+    // document.getElementById('historyHourId').innerHTML = hour;
+
+    // //Atualiza lista visivel do histórico
+    // document.getElementById('historyListId').innerHTML = '';
+    // history.stack.forEach((site) => {
+    //   let url = document.createElement('p');
+    //   url.appendChild(document.createTextNode(site));
+    //   document.getElementById('historyListId').appendChild(url);
+
+    //   //Registrar o horario
+    //   const now = new Date();
+    //   const hours = now.getHours(); // Gets the current hour (0-23)
+    //   const minutes = now.getMinutes(); // Gets the current minutes (0-59)
+
+    //   let hora = document.createElement('p');
+    //   hora.appendChild(document.createTextNode(site));
+    //   document.getElementById('historyHourId').appendChild(hora);
+    // });
   // });
 }
 
