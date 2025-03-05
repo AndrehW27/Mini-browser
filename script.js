4e30d3a4e3450980dc2ea19e696bc8137da1ff6f
AndrehW27 <andrehwillian@hotmail.com>
2025-03-05 20:00:14 -0300

    historico v1.1

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ const closeTab = () => {
 }
 
 const goBack = () => {
+  // history.peekHistory()
   console.log('goBack');
-  document.getElementById('urlInputId').value = sites[0].url;
-  document.getElementById('contentImgId').src = sites[0].img;
-  document.getElementById('tabIconId').src = sites[0].icon;
+  // document.getElementById('urlInputId').value = history.peekHistory().url;
+
+
+  // // console.log("peekHistory: ", history.peekHistory());
+  // console.log("History (Stack): ", history.stack);
 }
   
 const goForward = () => {
@@ -30,13 +33,13 @@ const refreshPage = () => {
 }
   
 const goHome = () => {
-    console.log('goHome');
-    document.getElementById('titleId').innerText = sites[0].title;
-    document.getElementById('contentImgId').src = sites[0].img;
-    document.getElementById('tabIconId').src = sites[0].icon;  
-    document.getElementById('urlInputId').value = sites[0].url;  
+    // console.log('goHome');
+    document.getElementById('titleId').innerText = sites.queue[0].title;
+    document.getElementById('contentImgId').src = sites.queue[0].img;
+    document.getElementById('tabIconId').src = sites.queue[0].icon;  
+    document.getElementById('urlInputId').value = sites.queue[0].url;  
 
-    let url = sites[0].url;
+    let url = sites.queue[0].url;
     let hour = day+'/'+month+' às '+hours+':'+minutes;
 
     let homeSite = {
@@ -68,14 +71,14 @@ const goHome = () => {
   
 const minimize = () => {
   console.log('Minimize');  
-  document.getElementById('containerId').style.height = '50vh';
-  document.getElementById('containerId').style.width = '50vw';
+  // document.getElementById('containerId').style.height = '50vh';
+  // document.getElementById('containerId').style.width = '50vw';
 }
 
 const maxmize = () => {
   console.log('Maxmize');
-  document.getElementById('containerId').style.height = '90vh';
-  document.getElementById('containerId').style.width = '95vw';
+  // document.getElementById('containerId').style.height = '90vh';
+  // document.getElementById('containerId').style.width = '95vw';
 }
 
 const closewindow = () => {
@@ -106,8 +109,10 @@ const popHistory = () => {
   const list = document.getElementById('historyListId');
   const url = document.getElementById('new-paragraph');
   const hour = document.getElementById('new-hour-paragraph');
-  list.removeChild(url);
-  list.removeChild(hour);
+  if (list.hasChildNodes()) {
+    list.removeChild(url);
+    list.removeChild(hour);
+  }
 }
 
 const clearHistory = () => {
@@ -120,15 +125,51 @@ const clearHistory = () => {
 //SITES/HISTORY OBJECT LISTS
 // ==============================
 
-var sites = [];
-sites.push({title: 'Google', url: 'https://www.google.com', icon: 'https://www.google.com/favicon.ico',img:'./assets/google-dev.png'});
-sites.push({title: 'Facebook', url: 'https://www.facebook.com', icon: 'https://www.facebook.com/favicon.ico',img:'./assets/facebook-img.png'});
-sites.push({title: 'Instagram', url: 'https://www.instagram.com', icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',img:'./assets/insta.avif'});
-sites.push({title: 'YouTube', url: 'https://www.youtube.com', icon: 'https://www.youtube.com/favicon.ico',img:'./assets/youtube-logo.jpeg'});
+//SITES QUEUE
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
 
-console.log("Sites: ", sites);
+  enqueue(site) {
+    this.queue.push(site);
+  }
 
-//HISTORY
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  peek() {
+    return this.queue[0];
+  }
+
+  size() {
+    return this.queue.length;
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+}
+
+let sites = new Queue();
+
+sites.enqueue({title: 'Google', url: 'https://www.google.com', icon: 'https://www.google.com/favicon.ico',img:'./assets/google-dev.png'}); 
+sites.enqueue({title: 'Facebook', url: 'https://www.facebook.com', icon: 'https://www.facebook.com/favicon.ico',img:'./assets/facebook-img.png'});
+sites.enqueue({title: 'Instagram', url: 'https://www.instagram.com', icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',img:'./assets/insta.avif'});
+sites.enqueue({title: 'YouTube', url: 'https://www.youtube.com', icon: 'https://www.youtube.com/favicon.ico',img:'./assets/youtube-logo.jpeg'});
+
+console.log("Sites (Queue): ", sites.queue);
+
+// var sites = [];
+// sites.push({title: 'Google', url: 'https://www.google.com', icon: 'https://www.google.com/favicon.ico',img:'./assets/google-dev.png'});
+// sites.push({title: 'Facebook', url: 'https://www.facebook.com', icon: 'https://www.facebook.com/favicon.ico',img:'./assets/facebook-img.png'});
+// sites.push({title: 'Instagram', url: 'https://www.instagram.com', icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',img:'./assets/insta.avif'});
+// sites.push({title: 'YouTube', url: 'https://www.youtube.com', icon: 'https://www.youtube.com/favicon.ico',img:'./assets/youtube-logo.jpeg'});
+
+
+
+//HISTORY STACK
 class HistoryStack {
   // constructor(url, hour) {
   //   this.stack = [
@@ -142,7 +183,7 @@ class HistoryStack {
 
   pushHistory(site) {
     this.stack.push(site);
-    console.log("history: ", this.stack);
+    console.log("History (Stack): ", this.stack);
   }
 
   popHistory() {
@@ -151,8 +192,7 @@ class HistoryStack {
   }
 
   peekHistory() {
-    return this.stack[this.stack.length - 1];
-    console.log("history: ", this.stack);
+    return this.stack[this.stack.length - 2];
   }
 
   clearHistory() {
@@ -163,12 +203,37 @@ class HistoryStack {
 
 const history = new HistoryStack();
 
-console.log("history: ", history);
+let url = sites.queue[0].url;
+let hour = day+'/'+month+' às '+hours+':'+minutes;
+
+let historySite = {
+  url: url,
+  hour: hour
+};
+
+history.pushHistory(historySite);
+
+  //Atualiza lista visivel do histórico (criando um <p>)  
+  const list = document.getElementById('historyListId');
+
+  //HORA
+  const hourParagraph = document.createElement('p');
+  hourParagraph.id = 'new-hour-paragraph'
+  hourParagraph.classList.add('hora');
+  hourParagraph.textContent = hour;
+  list.prepend(hourParagraph);
+
+  //URL 
+  const urlParagraph = document.createElement('p');
+  urlParagraph.id = 'new-paragraph'
+  urlParagraph.classList.add('history-site');
+  urlParagraph.textContent = url;
+  list.prepend(urlParagraph);
 
 //Setando a home para google
-document.getElementById('urlInputId').value = sites[0].url;
-document.getElementById('contentImgId').src = sites[0].img;
-document.getElementById('tabIconId').src = sites[0].icon;
+document.getElementById('urlInputId').value = sites.queue[0].url;
+document.getElementById('contentImgId').src = sites.queue[0].img;
+document.getElementById('tabIconId').src = sites.queue[0].icon;
 
 //Selecionar tudo ao clicar na url
 document.getElementById('urlInputId').addEventListener('click', () => {
@@ -191,34 +256,52 @@ document.getElementById('urlInputId').addEventListener('keydown', (event) => {
   }
 });
 
-//Ouvindo valor do input da url
+//Ouvindo SEMPRE valor do input da url
+// document.getElementById('urlInputId').addEventListener('input', (event) => {
+//   let input =  event.target.value;
+
+//   let listOfUrls = history.stack.map((site) => site.url);
+
+//   if (listOfUrls.some(item => item.includes(input))) {
+//     const urlList = document.getElementById('urlHistoryBoxId');
+
+//     //HORA
+//     const historyItem = document.createElement('p');
+//     historyItem.id = 'new-history-item-id'
+//     historyItem.classList.add('history-site');
+//     historyItem.textContent = 'item';
+//     urlList.prepend(historyItem);
+//   }
+// });
+
+//O que fazer ao mudar a url e teclar enter
 const changeUrl = () => {
   // document.getElementById('urlInputId').addEventListener('input', (event) => {
     let input = document.getElementById('urlInputId');
     let currentValue = input.value;
 
+  //mudando a tela e a aba
     if (currentValue.includes('goog')) {
-      input.value = currentValue.replace(currentValue, sites[0].url);
-      document.getElementById('titleId').innerText = sites[0].title;
-      document.getElementById('contentImgId').src = sites[0].img;
-      document.getElementById('tabIconId').src = sites[0].icon;  
+      input.value = currentValue.replace(currentValue, sites.queue[0].url);
+      document.getElementById('titleId').innerText = sites.queue[0].title;
+      document.getElementById('contentImgId').src = sites.queue[0].img;
+      document.getElementById('tabIconId').src = sites.queue[0].icon;  
     } else if (currentValue.includes('face')) {
-      input.value = currentValue.replace(currentValue, sites[1].url);
-      document.getElementById('titleId').innerText = sites[1].title;
-      document.getElementById('contentImgId').src = sites[1].img;
-      document.getElementById('tabIconId').src = sites[1].icon;   
+      input.value = currentValue.replace(currentValue, sites.queue[1].url);
+      document.getElementById('titleId').innerText = sites.queue[1].title;
+      document.getElementById('contentImgId').src = sites.queue[1].img;
+      document.getElementById('tabIconId').src = sites.queue[1].icon;   
     } else if (currentValue.includes('insta')) {
-      input.value = currentValue.replace(currentValue, sites[2].url);
-      document.getElementById('titleId').innerText = sites[2].title;
-      document.getElementById('contentImgId').src = sites[2].img;
-      document.getElementById('tabIconId').src = sites[2].icon;  
+      input.value = currentValue.replace(currentValue, sites.queue[2].url);
+      document.getElementById('titleId').innerText = sites.queue[2].title;
+      document.getElementById('contentImgId').src = sites.queue[2].img;
+      document.getElementById('tabIconId').src = sites.queue[2].icon;  
     } else if (currentValue.includes('you')) {
-      input.value = currentValue.replace(currentValue, sites[3].url);
-      document.getElementById('titleId').innerText = sites[3].title;
-      document.getElementById('contentImgId').src = sites[3].img;
-      document.getElementById('tabIconId').src = sites[3].icon;  
+      input.value = currentValue.replace(currentValue, sites.queue[3].url);
+      document.getElementById('titleId').innerText = sites.queue[3].title;
+      document.getElementById('contentImgId').src = sites.queue[3].img;
+      document.getElementById('tabIconId').src = sites.queue[3].icon;  
     }
-
 
     //Rsgitro de site no historico
     let url = input.value;
@@ -228,6 +311,10 @@ const changeUrl = () => {
       url: url,
       hour: hour
     };
+
+    // let setLista = new Set(history.stack.map((site) => site.url));
+    // console.log("setLista: ", setLista);
+    
 
     //Preenche o history stack
     history.pushHistory(historySite);
@@ -248,6 +335,12 @@ const changeUrl = () => {
     urlParagraph.classList.add('history-site');
     urlParagraph.textContent = url;
     list.prepend(urlParagraph);
+
+    //CLOSE
+    // const x = document.createElement('button');
+    // x.textContent = 'X';
+    // urlParagraph.appendChild(x);
+
 
 
 
